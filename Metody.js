b0dbc8405fb49febfb0e37621075012e1af47d44
Adam Pawlikowski <pawlikowskiadam@endor.local>
2020-02-17 10:06:22 +0100

    17.02.2020

---
+++ Metody.js
@@ -0,0 +1,12 @@
+Object.prototype.NazwaMetody = function() {
+
+}
+
+arr.NazwaMetody() //Wywoływanie metody prototypowej
+
+function Klasa(pole) {
+    this.nazwaPola = pole;
+    this.NazwaMetody = function(){ //tworzenie metody
+
+    } 
+}
